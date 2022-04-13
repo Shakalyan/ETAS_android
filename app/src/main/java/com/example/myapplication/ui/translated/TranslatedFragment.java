33838956d1d90c3c9846001896b3e2446c6b0452
maselof <mr.gindeev@gmail.com>
2022-04-13 23:38:39 +0300

    translated_fragment_front

--- app/src/main/java/com/example/myapplication/ui/translated/TranslatedFragment.java
+++ app/src/main/java/com/example/myapplication/ui/translated/TranslatedFragment.java
@@ -1,48 +1,42 @@
 package com.example.myapplication.ui.translated;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.myapplication.R;
+import com.example.myapplication.databinding.FragmentTranslatedBinding;
 
 public class TranslatedFragment extends Fragment {
 
     private Button btn_translated;
     private TextView translated_text;
+    private FragmentTranslatedBinding binding;
 
-    public TranslatedFragment(){
-        super(R.layout.fragment_translated);
-        btn_translated = btn_translated.findViewById(R.id.btn_translated);
-        translated_text = translated_text.findViewById(R.id.translated_text);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        TranslatedViewModel wordsViewModel =
+                new ViewModelProvider(this).get(TranslatedViewModel.class);
+
+        binding = FragmentTranslatedBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
+
+        btn_translated = root.findViewById(R.id.btn_translated);
+        translated_text = root.findViewById(R.id.translated_text);
         btn_translated.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 translated_text.setText("работает");
             }
         });
-    }
 
-    /*private FragmentTranslatedBinding binding;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        TranslatedViewModel translatedViewModel =
-                new ViewModelProvider(this).get(TranslatedViewModel.class);
-
-        binding = FragmentTranslatedBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-
-        final TextView textView = binding.textSlideshow;
-        translatedViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
-        binding = null;
-    }*/
 }
