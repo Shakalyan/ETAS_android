aef49aa64046528afb3c82a0b4f00248907fd3ed
Shakalyan <vdovitg@gmail.com>
2022-04-18 21:39:01 +0300

    Add tests starting interface

--- app/src/main/java/com/example/myapplication/ui/tests/TestsFragment.java
+++ app/src/main/java/com/example/myapplication/ui/tests/TestsFragment.java
@@ -1,8 +1,22 @@
 package com.example.myapplication.ui.tests;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.ViewFlipper;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.example.myapplication.R;
+import com.example.myapplication.databinding.FragmentTestsBinding;
 
 public class TestsFragment extends Fragment {
 
@@ -10,7 +24,11 @@ public class TestsFragment extends Fragment {
         super(R.layout.fragment_tests);
     }
 
-    /*private FragmentTestsBinding binding;
+    private FragmentTestsBinding binding;
+
+    private ViewFlipper testsFlipper;
+    private Spinner testsSpinner;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -20,8 +38,15 @@ public class TestsFragment extends Fragment {
         binding = FragmentTestsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.helloTests;
-        testsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        testsFlipper = root.findViewById(R.id.testsFlipper);
+        testsSpinner = root.findViewById(R.id.tests_spinner);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(requireContext(), R.array.tests_names, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        testsSpinner.setAdapter(adapter);
+
+
+
         return root;
     }
 
@@ -29,5 +54,5 @@ public class TestsFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
-    }*/
+    }
 }
